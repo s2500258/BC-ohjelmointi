54f108e1a10353d43896e795af928397b403652e
s2500258 <akulov.alex.wrk@gmail.com>
2025-04-09 15:02:51 +0300

    09.04.2025 2

---
+++ JS-kurssi/03_viikko/DOM_01/DOM_01A/script.js
@@ -0,0 +1,7 @@
+/* 
+Kirjoita JavaScript-koodi, joka valitsee sisällön tunnuksen käyttämällä getElementById-metodia.
+Kirjoita funktio, joka päivittää valitun elementin tekstisisällön painamalla nappia tekstiksi "Hei, maailma!".
+*/
+document.getElementById("press").addEventListener("click", function(){
+    document.getElementById("content").textContent="Hey!!!"
+});
